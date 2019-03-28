daaa8db473dc6c54954141bec03e40753ed97874
Skhumbuzo <1608194@student.wits.ac.za>
2019-03-28 14:16:23 +0200

    SRC_POLL updated

--- src_poll.php
+++ src_poll.php
@@ -20,7 +20,7 @@ switch ($choice){
         $poll_date = $_REQUEST[Constants::POLL_DATE];
         $poll_time = $_REQUEST[Constants::POLL_TIME];
 
-        $stmt = "INSERT INTO ".Constants::SRC_POLL_TABLE." VALUES (:PI, :MU, :PT, :PD, :PC, :PDD, PTT)";
+        $stmt = "INSERT INTO ".Constants::SRC_POLL_TABLE." VALUES (:PI, :MU, :PT, :PD, :PC, :PDD, :PTT)";
         $args = array(":PI" => $poll_id, ":MU" => $member_username, ":PT" => $poll_title, ":PD" => $poll_desc,
             ":PC" => $poll_choices, ":PDD" => $poll_date, ":PTT" => $poll_time);
         $databaseManager -> executeStatement($stmt, $args);
