a7d218e5c91d0d21736998f490f201b43fde169f
Skhumbuzo <1608194@student.wits.ac.za>
2019-03-28 03:07:09 -0700

    SRC_POLL

--- SRC_POLL.php
+++ SRC_POLL.php
@@ -12,7 +12,7 @@ switch ($choice){
         $member_username = $_REQUEST[Constants::SRC_MEMBER_USER];
         $poll_id = $_REQUEST[Constants::POLL_ID];
         $poll_message = $_REQUEST[Constants::POLL_MSG];
-        $poll_choices = $_REQUEST[Constants::POLL_CHOICE0];
+        $poll_choices = $_REQUEST[Constants::POLL_CHOICE];
         $poll_date = $_REQUEST[Constants::POLL_DATE];
         $poll_time = $_REQUEST[Constants::POLL_TIME];
 
