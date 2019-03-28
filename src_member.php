4eabd29b12950af1934577883d7334e43b296c2a
MbusoMakitla <1699140@students.wits.ac.za>
2019-03-28 14:45:45 +0200

    request password

--- src_member.php
+++ src_member.php
@@ -58,7 +58,7 @@ switch($choice){
     case Constants::READ_ALL_ACTIVITIES:
             //Mbuso's section
             // upadte will be posted by 11
-
+            $member_username = $_REQUEST[Constants::SRC_MEMBER_USER];
 
 
 
