8a4f35f9d6a031b1bf44c9d0682985419d4c59d6
PermanentPortionX <1627982@students.wits.ac.za>
2019-03-28 10:07:14 +0200

    Updated comment

--- student_comment.php
+++ student_comment.php
@@ -31,6 +31,7 @@ switch ($choice){
 
         $sql = "SELECT * FROM ".Constants::STUD_COMMENT_TABLE." WHERE ".Constants::ACTIVITY_ID." = :AI";
         $args = array(":AI" => $activity_id);
+        $databaseManager -> executeFetchStatement($sql, $args);
         break;
 
         //comment only
