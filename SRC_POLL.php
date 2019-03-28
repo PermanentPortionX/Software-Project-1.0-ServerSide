edf868794341e4c8c1ee8194dcc4f1d23e70eb65
Skhumbuzo <1608194@student.wits.ac.za>
2019-03-28 02:38:08 -0700

    Inprogress: SRC_POLL

--- SRC_POLL.php
+++ SRC_POLL.php
@@ -1,3 +1,8 @@
 <?php
 
+require_once("WitsSrcConnectDatabaseManager.php");
+require_once("Constants.php");
 
+$databaseManager = new WitsSrcConnectDatabaseManager();
+
+$choice = $_REQUEST[Constants::ACTION];
